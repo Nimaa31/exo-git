d6955e4059480cb9c366032cb14cddc1608bafa3
Amin <amin.eddine31@icloud.com>
2024-11-12 11:57:30 +0100

    thrid

--- age.py
+++ age.py
@@ -22,6 +22,23 @@ def temps_depuis_majorite(date_naissance):
     else:
         return 0  # Pas encore majeur
 
+def temps_depuis_permis(date_permis):
+    """Calcule le temps écoulé depuis l'obtention du permis."""
+    aujourd_hui = datetime.now()
+    annees = aujourd_hui.year - date_permis.year
+    mois = aujourd_hui.month - date_permis.month
+    jours = aujourd_hui.day - date_permis.day
+
+    # Ajustement pour les mois et jours négatifs
+    if jours < 0:
+        mois -= 1
+        jours += 30  # Approximation du nombre de jours par mois
+    if mois < 0:
+        annees -= 1
+        mois += 12
+
+    return annees, mois
+
 # Exemple d'utilisation
 date_naissance = datetime(2002, 7, 22)
 age_plus_10 = age_dans_x_ans(date_naissance, 10)
@@ -32,3 +49,11 @@ print(f"Dans 10 ans vous aurez = {age_plus_10} ans.")
 temps_majorite = temps_depuis_majorite(date_naissance)
 if temps_majorite > 0:
     print(f"Cela fait {temps_majorite} ans que vous êtes majeur(e).")
+
+# Demande à l'utilisateur la date d'obtention de son permis
+date_permis_str = input("Entrez la date d'obtention de votre permis (au format JJ/MM/AAAA) : ")
+date_permis = datetime.strptime(date_permis_str, "%d/%m/%Y")
+
+# Calcul du temps écoulé depuis l'obtention du permis
+annees_permis, mois_permis = temps_depuis_permis(date_permis)
+print(f"Cela fait {annees_permis} ans et {mois_permis} mois que vous avez le permis.")
